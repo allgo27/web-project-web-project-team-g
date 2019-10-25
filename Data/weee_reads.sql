3506ae74f451d762bb7a85f1e2546c5414081cdd
Lab User <labuser@wcc138-19r.build.carleton.edu>
2019-10-25 17:09:21 -0500

    what???

--- Data/weee_reads.sql
+++ Data/weee_reads.sql
@@ -1,7 +1,5 @@
 DROP TABLE IF EXISTS books;
 CREATE TABLE books (
-  book_id text,
-  goodreads_book_id text,
   authors text,
   original_publication_year text,
   original_title text,
@@ -9,6 +7,8 @@ CREATE TABLE books (
   average_rating text,
   ratings_count text,
   image_url text
+  book_id text,
+  goodreads_book_id text,
 );
 
 DROP TABLE IF EXISTS ratings;
