af7783d8f36ce111444854d968087746600da93f
Lab User <labuser@wcc138-20r.build.carleton.edu>
2019-10-24 20:22:30 -0500

    altered sql file

--- Data/weee_reads.sql
+++ Data/weee_reads.sql
@@ -1,33 +1,33 @@
 DROP TABLE IF EXISTS books;
 CREATE TABLE books (
   authors varchar(300),
-  original_publication_year int(4),
+  original_publication_year int,
   original_title varchar(300),
   title varchar(300),
   average_rating float(3, 2),
-  ratings_count int (10)
+  ratings_count int,
   image_url varchar(300),
   small_image_url varchar(300),
-  book_id int (5)
+  book_id int
 );
 
 DROP TABLE IF EXISTS ratings;
 CREATE TABLE ratings (
-  user_id int(4),
-  book_id int(5),
-  rating int(1)
+  user_id int,
+  book_id int,
+  rating int
 );
 
 DROP TABLE IF EXISTS book_tags;
 CREATE TABLE book_tags (
-	goodreads_book_id int(4),
-	tag_id int(6),
-	tagcount int(10)
+	goodreads_book_id int,
+	tag_id int,
+	tagcount int
 );
 
 DROP TABLE IF EXISTS tags;
 CREATE TABLE tags (
-	tag_id int(6),
+	tag_id int,
 	tag_name varchar(100)
 )
 
