43039a3bc9db1fcf98274d50d99c9c2cc161f4e6
Lab User <labuser@wcc138-19r.build.carleton.edu>
2019-10-25 16:59:47 -0500

    tried casting book_id

--- Data/datasource.py
+++ Data/datasource.py
@@ -121,7 +121,7 @@ class DataSource:
         '''
         try:
             cursor = self.connection.cursor()
-            query = "SELECT title FROM books WHERE book_id=" + str(bookID)
+            query = "SELECT title FROM books WHERE book_id='1'"
             cursor.execute(query)
             return cursor.fetchall()
         
