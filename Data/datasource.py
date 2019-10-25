1bf5c32bd64a68f345be72162787be725bea2437
Lab User <labuser@wcc138-19r.build.carleton.edu>
2019-10-25 16:12:51 -0500

    updated datasource.py file with the main function

--- Data/datasource.py
+++ Data/datasource.py
@@ -2,35 +2,52 @@ import psycopg2
 import getpass
 
 class DataSource:
-	'''
+    '''
 	DataSource executes all of the queries on the database.
 	It also formats the data to send back to the frontend, typically in a list
 	or some other collection or object.
-	'''
-
+    '''
+    
     def __init__(self):
-        pass
+        self.connection = None
+        
+    def connect(self, user, password):
+        '''
+        Establishes a connection to the database with the following credentials:
+            user - username, which is also the name of the database
+            password - the password for this database on perlman
 
+        Returns: a database connection.
+
+        Note: exits if a connection cannot be established.
+        Copied from code by Amy Dalal (and used with permission)
+        '''
+        try:
+            self.connection = psycopg2.connect(database=user, user=user, password=password)
+        except Exception as e:
+            print("Connection error: ", e)
+            exit()
+            
+        return 
+    
     def getBookID(self, title, author):
         '''
         Returns the database "book_id" number for specified title by specified author
-
         PARAMETERS:
             title - the book's title
             author - the book's author
-
         RETURN:
             Database "book_id" number
         '''
         return ""
-
-	def getGoodreadsBookID(self, title, author):
+    
+    
+    
+    def getGoodreadsBookID(self, title, author):
         '''
         get Goodreads "goodreads_book_id" number based off of bookID
-
         PARAMETERS:
             bookID - the database ID number for the book
-
         RETURN:
             Database "goodreads_book_id" number
         '''
@@ -39,7 +56,6 @@ class DataSource:
     def getBookRating(self, bookID):
         '''
         Returns tuple of average rating of book and number of people who have rated it overall
-
         PARAMETERS:
             bookID - the database ID number for the book 
         
@@ -53,7 +69,6 @@ class DataSource:
         Returns a list of top three tags of given book according to goodreadsBookID
         PARAMETERS:
             goodreadsBookID - the goodreads ID number for the book
-
         RETURN:
             a tuple of top 3 tags for a given book
         '''
@@ -64,71 +79,75 @@ class DataSource:
         Returns the url of an image of specified book
         PARAMETERS:
             bookID - the database ID number for the book
-
         RETURN:
             a string containing the URL of an image of the specified book
         '''
-        try:
-			cursor = connection.cursor()
-			query = "SELECT	image_url FROM books WHERE book_id=" + str(bookID) 
-			cursor.execute(query)
-			return cursor.fetchall()
-
-		except Exception as e:
-			print ("Something went wrong when executing the query: ", e)
-			return None
         
-     def getAuthor(self, bookID):
+        try:
+            cursor = connection.cursor()
+            query = "SELECT image_url FROM books WHERE book_id=" + str(bookID)
+            cursor.execute(query)
+            return cursor.fetchall()
+        
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return
+        
+    def getAuthor(self, bookID):
         '''
         Returns the author of specified book
         PARAMETERS:
             bookID - the database ID number for the book
-
         RETURN:
             a string containing the name of the author of the specified book
         '''
-        
         try:
-			cursor = connection.cursor()
-			query = "SELECT	authors FROM books WHERE book_id=" + str(bookID) 
-			cursor.execute(query)
-			return cursor.fetchall()
-
-		except Exception as e:
-			print ("Something went wrong when executing the query: ", e)
-			return None
+            cursor = connection.cursor()
+            query = "SELECT	authors FROM books WHERE book_id=" + str(bookID) 
+            cursor.execute(query)
+            return cursor.fetchall()
         
-	def getTitle(self, bookID):
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return 
+        
+    def getTitle(self, bookID):
         '''
         Returns the title of specified book
         PARAMETERS:
             bookID - the database ID number for the book
-
         RETURN:
             a string containing the name of the specified book
         '''
-        
         try:
-			cursor = connection.cursor()
-			query = "SELECT	title FROM books WHERE book_id=" + str(bookID) 
-			cursor.execute(query)
-			return cursor.fetchall()
-
-		except Exception as e:
-			print ("Something went wrong when executing the query: ", e)
-			return None
+            cursor = self.connection.cursor()
+            query = "SELECT title FROM books WHERE book_id=" + str(bookID) 
+            cursor.execute(query)
+            return cursor.fetchall()
+        
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return None
         
     
     def getBookRecommendation(self, bookID1, bookID2):
-    	'''
-    	Returns the database bookIDs of three recommended books
-    	PARAMETERS:
-    		bookID1 - the first book to base recommendation off of
-    		bookID2 - the second book to base recommendation off of
-    		
-    	RETURN:
-    		a tuple of three database bookIDs
-    	'''
-    	return ()
-    	
+        '''
+        Returns the database bookIDs of three recommended books
+        PARAMETERS:
+            bookID1 - the first book to base recommendation off of
+            bookID2 - the second book to base recommendation off of
+            
+        RETURN:
+        a tuple of three database bookIDs
+        '''
+        return ()
     
+    
+def main():
+    data = DataSource()
+    data.connect("bruelle", "spider268awesome")
+    mylist = data.getTitle(1)
+    print(mylist)
+    
+    
+main()
