3d7127b9d120b434bcbd7023d6df64de1899b235
Lab User <labuser@wcc138-19r.build.carleton.edu>
2019-10-25 19:06:07 -0500

    try this

--- Data/datasource.py
+++ Data/datasource.py
@@ -146,9 +146,9 @@ class DataSource:
 def main():
     data = DataSource()
     data.connect("allgoodm", "cow245happy")
-   # answer = data.getTitle(1))
-    #print(answer[])
-    print(data.getTitle(1))
+    answer = data.getTitle(1))
+    print(answer[0])
+    #print(data.getTitle(1))
     #mylist = []
     #mylist.append(data.getTitle(1))
     #mylist.append(data.getAuthor(1))
