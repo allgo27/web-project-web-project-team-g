7f22a6984f292f63c9e741d17e9e7aaf0140193d
Lab User <labuser@wcc138-05r.build.carleton.edu>
2019-10-30 16:30:04 -0500

    adding tester

--- Data/datasourceTestr.py
+++ Data/datasourceTestr.py
@@ -5,17 +5,17 @@ from datasource import *
 class MyTestCase(unittest.TestCase):
     def test_basic(self):
         ds = DataSource()
-        booklist = getBookRecID(1,2)
+        booklist = ds.getBookRecID(1,2)
         self.assertEqual(len(booklist), 3)
 
     def test_few_fans(self):
         ds = DataSource()
-        booklist = getBookRecID(10001,30000)
+        booklist = ds.getBookRecID(10001,30000)
         self.assertEqual(len(booklist), 3)
 
     def test_no_common_books(self):
         ds = DataSource()
-        booklist = getBookRecID(10001, 30000)
+        booklist = ds.getBookRecID(10001, 30000)
         self.assertEqual(len(booklist), 3)
 
 if __name__ == '__main__':
