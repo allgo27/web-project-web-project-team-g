1729a82c9e974c9a09dd6f90c4f8e364f149a362
Emily Bruell <bruelle@carleton.edu>
2019-10-30 22:58:45 -0500

    wednesday night algorithm adjustment

--- Data/datasourceTestr.py
+++ Data/datasourceTestr.py
@@ -8,16 +8,16 @@ class MyTestCase(unittest.TestCase):
         ds.connect("bruelle", "spider268awesome")
         booklist = ds.getBookRecID(1,2)
         self.assertEqual(len(booklist), 3)
-
-    def test_few_fans(self):
-        ds = DataSource()
-        booklist = ds.getBookRecID(10001,30000)
-        self.assertEqual(len(booklist), 3)
-
-    def test_no_common_books(self):
-        ds = DataSource()
-        booklist = ds.getBookRecID(10001, 30000)
-        self.assertEqual(len(booklist), 3)
+#
+#   '''' def test_few_fans(self):
+#        ds = DataSource()
+#        booklist = ds.getBookRecID(10001,30000)
+#        self.assertEqual(len(booklist), 3)
+#
+#    def test_no_common_books(self):
+#        ds = DataSource()
+#        booklist = ds.getBookRecID(10001, 30000)
+#        self.assertEqual(len(booklist), 3
 
 if __name__ == '__main__':
     unittest.main()
