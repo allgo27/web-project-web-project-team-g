b323c4b9ab90c704132c908079fb3582f0e97cbb
yime2 <yime2@cmc306-02.mathcs.carleton.edu>
2019-11-10 20:44:40 -0600

    our fruit experiment

--- backend/Data/webapp.py
+++ backend/Data/webapp.py
@@ -11,25 +11,42 @@ db.connect("yime2", "tablet389cow")
 
 @app.route('/')
 def homePage():
-    print("can we even print?")
+    
     return render_template('WRhomepage.html')
     
 
 @app.route('/midresults')
 def midresultsBooks():
     result = request.form
-    print(result)
+    firstbook = result['firstbook']
+    secondbook = result['secondbook']
 #    firstbook = request.args.get('firstbook')
 #    secondbook = request.args.get('secondbook')
-#    firstBookAuthors = db.getPossibleAuthors(firstbook)
-#    print(firstBookAuthors)
-#    secondBookAuthors = db.getPossibleAuthors(secondbook)
-#    print(secondBookAuthors)
-    potentialBooks = [
-        {'title': 'apple', 'author': 'orangutan', 'optionNum':i},
-        {'title': 'banana', 'author': 'jim'},
-        {'title': 'pear', 'author': 'terry'}
-    ]
+    firstBookAuthors = db.getPossibleAuthors(firstbook)
+    secondBookAuthors = db.getPossibleAuthors(secondbook)
+    potentialBooks = list ()
+    i = 0
+    for author in firstBookAuthors:
+        optionDict = {}
+        optionDict['title'] = firstbook
+        optionDict['author'] = author[0] #if we're having problems indexing out of range you don't actually need the [0] but I'm pretty sure we do
+        optionDict['optionNum'] = 'option'+str(i)
+        potentialBooks.append(optionDict)
+        i += 1
+        
+    for author in secondBookAuthors:
+        optionDict = {}
+        optionDict['title'] = secondbook
+        optionDict['author'] = author[0] #if we're having problems indexing out of range you don't actually need the [0] but I'm pretty sure we do
+        optionDict['optionNum'] = 'option'+str(i)
+        potentialBooks.append(optionDict)
+        i += 1
+        
+#    potentialBooks = [
+#        {'title': 'apple', 'author': 'orangutan', 'optionNum':i},
+#        {'title': 'banana', 'author': 'jim'},
+#        {'title': 'pear', 'author': 'terry'}
+#    ]
 
     return render_template('midresults.html',
                            books=potentialBooks)
