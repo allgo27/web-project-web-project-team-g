dd776efef98f4826c18b9621a4a98cee5f3a20d9
allgoodm <allgoodm@cmc306-01.mathcs.carleton.edu>
2019-11-12 10:59:43 -0600

    merge conflicts resolved

--- backend/Data/webapp.py
+++ backend/Data/webapp.py
@@ -17,19 +17,6 @@ def homePage():
 
 @app.route('/midresults/', methods=['POST','GET']) #added the methods=... part with Andy
 def midresultsBooks():
-<<<<<<< HEAD
-    firstBook = request.args.get('firstbook')
-    secondBook = request.args.get('secondbook')
-    firstPossible = getPossibleBooks(firstBook)
-    secondPossible = getPossibleBooks(secondBook)
-
-    
-    potentialBooks = [
-        {'title': 'apple', 'author': 'orangutan', 'optionNum':i},
-        {'title': 'banana', 'author': 'jim'},
-        {'title': 'pear', 'author': 'terry'}
-    ]
-=======
     if request.method == 'POST': #added the 'POST' and if statement. This code is from his flask app that I have in email. We can look at this for help.
     
         result = request.form
@@ -56,22 +43,7 @@ def midresultsBooks():
     #        optionDict['optionNum'] = 'option'+str(i)
     #        potentialBooks.append(optionDict)
     #        i += 1
->>>>>>> f81a9a17d10e5a75a11a6550c01cb6dd9dafd535
 
-
-<<<<<<< HEAD
-#Taken from Amy's slack as an example
-@app.route('/resultletter', methods = ['POST', 'GET'])
-def resultLetter():
-    if request.method == 'POST':
-        result = request.form
-        ds = datasource.DataSource()
-        description = "Showing all names beginning with " + result.get("Letter") + " sorted alphabetically"
-        result = ds.getLetter(result.get("Letter"))
-        return render_template('result.html', result = result, description = description)
-
-def main:
-=======
     #    potentialBooks = [
     #        {'title': 'apple', 'author': 'orangutan', 'optionNum':option0},
     #        {'title': 'banana', 'author': 'jim'},
@@ -83,7 +55,6 @@ def main:
 
 
 def main():
->>>>>>> f81a9a17d10e5a75a11a6550c01cb6dd9dafd535
     if len(sys.argv) != 3:
         print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
         exit()
@@ -93,13 +64,10 @@ def main():
     app.run(host=host, port=port)
     
 
-<<<<<<< HEAD
+
     
 
 if __name__ == '__main__':
     main()
     
-=======
-if __name__ == '__main__':
-    main()
->>>>>>> f81a9a17d10e5a75a11a6550c01cb6dd9dafd535
+
