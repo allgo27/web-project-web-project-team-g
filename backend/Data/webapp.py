aec10fb28c9bd6c918e5f35856943678747baf4e
yime2 <yime2@cmc307-04.mathcs.carleton.edu>
2019-11-11 22:17:22 -0600

    IDK

--- backend/Data/webapp.py
+++ backend/Data/webapp.py
@@ -15,43 +15,45 @@ def homePage():
     return render_template('WRhomepage.html')
     
 
-@app.route('/midresults', methods=['POST','GET'])
+@app.route('/midresults/', methods=['POST','GET'])
 def midresultsBooks():
-    print('hello')
-    result = request.form
-    firstbook = result['firstbook']
-    secondbook = result['secondbook']
-#    firstbook = request.args.get('firstbook')
-#    secondbook = request.args.get('secondbook')
-    firstBookAuthors = db.getPossibleAuthors(firstbook)
-    secondBookAuthors = db.getPossibleAuthors(secondbook)
-    potentialBooks = list ()
-    i = 0
-    for author in firstBookAuthors:
-        optionDict = {}
-        optionDict['title'] = firstbook
-        optionDict['author'] = author[0] #if we're having problems indexing out of range you don't actually need the [0] but I'm pretty sure we do
-        optionDict['optionNum'] = 'option'+str(i)
-        potentialBooks.append(optionDict)
-        i += 1
-        
-    for author in secondBookAuthors:
-        optionDict = {}
-        optionDict['title'] = secondbook
-        optionDict['author'] = author[0] #if we're having problems indexing out of range you don't actually need the [0] but I'm pretty sure we do
-        optionDict['optionNum'] = 'option'+str(i)
-        potentialBooks.append(optionDict)
-        i += 1
-        
+    if request.method == 'POST':
     
-#    potentialBooks = [
-#        {'title': 'apple', 'author': 'orangutan', 'optionNum':option0},
-#        {'title': 'banana', 'author': 'jim'},
-#        {'title': 'pear', 'author': 'terry'}
-#    ]
+        print('hello')
+        result = request.form
+        firstbook = result['firstbook']
+        secondbook = result['secondbook']
+    #    firstbook = request.args.get('firstbook')
+    #    secondbook = request.args.get('secondbook')
+        firstBookAuthors = db.getPossibleAuthors(firstbook)
+    #    secondBookAuthors = db.getPossibleAuthors(secondbook)
+    #    potentialBooks = list ()
+    #    i = 0
+    #    for author in firstBookAuthors:
+    #        optionDict = {}
+    #        optionDict['title'] = firstbook
+    #        optionDict['author'] = author[0] #if we're having problems indexing out of range you don't actually need the [0] but I'm pretty sure we do
+    #        optionDict['optionNum'] = 'option'+str(i)
+    #        potentialBooks.append(optionDict)
+    #        i += 1
+    #        
+    #    for author in secondBookAuthors:
+    #        optionDict = {}
+    #        optionDict['title'] = secondbook
+    #        optionDict['author'] = author[0] #if we're having problems indexing out of range you don't actually need the [0] but I'm pretty sure we do
+    #        optionDict['optionNum'] = 'option'+str(i)
+    #        potentialBooks.append(optionDict)
+    #        i += 1
 
-    return render_template('midresults.html',
-                           books=potentialBooks)
+
+    #    potentialBooks = [
+    #        {'title': 'apple', 'author': 'orangutan', 'optionNum':option0},
+    #        {'title': 'banana', 'author': 'jim'},
+    #        {'title': 'pear', 'author': 'terry'}
+    #    ]
+
+        return render_template('midresults.html',
+                               books=firstBookAuthors)
 
 
 def main():
