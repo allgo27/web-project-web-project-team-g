4f8d8df46a39dc2bfd1b5252376d427856e84b4c
Lab User <labuser@wcc138-09r.build.carleton.edu>
2019-11-13 17:28:14 -0600

    trying new things lots of fake files

--- backend/Data/fakewebapp.py
+++ backend/Data/fakewebapp.py
@@ -46,7 +46,7 @@ def homePage():
             {'title': 'banana', 'author': 'jim'},
             {'title': 'pear', 'author': 'terry'}
         ]
-        return redirect(url_for('midresults'), potentialBooks)
+        return redirect('http://perlman.mathcs.carleton.edu:5103/midresults.html', potentialBooks)
         #potentialBooks = makeTesterList()
     return render_template('WRhomepage.html')
 
