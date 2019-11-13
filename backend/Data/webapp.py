233afaf26ff074dd298004516575ba7812077a03
yime2 <yime2@cmc307-09.mathcs.carleton.edu>
2019-11-12 19:33:52 -0600

    trying to hardcode the html names?

--- backend/Data/webapp.py
+++ backend/Data/webapp.py
@@ -29,7 +29,7 @@ def fruit():
 
 @app.route('/midresults.html?firstbook=Lost&secondbook=The+Host', methods=['POST','GET']) #added the methods=... part with Andy
 def midresultsBooks():
-    if request.method == 'POST': #added the 'POST' and if statement. This code is from his flask app that I have in email. We can look at this for help.
+    #if request.method == 'POST': #added the 'POST' and if statement. This code is from his flask app that I have in email. We can look at this for help.
     
 #        result = request.form
 #        firstbook = result['firstbook']
