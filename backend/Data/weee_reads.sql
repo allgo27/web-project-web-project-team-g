4dc8426c364510c50f0f11c977f3547b25bd75f1
Emily Bruell <Emily@eduroam-181-63.dyn.carleton.edu>
2019-11-12 20:56:37 -0600

    adjusted sql

--- backend/Data/weee_reads.sql
+++ backend/Data/weee_reads.sql
@@ -1,32 +1,32 @@
 DROP TABLE IF EXISTS books;
 CREATE TABLE books (
-  authors text,
-  original_publication_year text,
-  original_title text,
-  title text,
-  average_rating text,
-  ratings_count text,
-  image_url text,
-  small_image_url text,
-  book_id text
+    book_id text,
+    goodreads_book_id text,
+    authors text,
+    original_publication_year text,
+    original_title text,
+    title text,
+    average_rating text,
+    ratings_count text,
+    image_url text,
 );
 
 DROP TABLE IF EXISTS ratings;
 CREATE TABLE ratings (
-  user_id text,
-  book_id text,
-  rating text
+    user_id text,
+    book_id text,
+    rating text
 );
 
 DROP TABLE IF EXISTS book_tags;
 CREATE TABLE book_tags (
-	goodreads_book_id text,
-	tag_id text,
-	tagcount text
+    goodreads_book_id text,
+    tag_id text,
+    tagcount text
 );
 
 DROP TABLE IF EXISTS tags;
 CREATE TABLE tags (
-	tag_id text,
-	tag_name text
+    tag_id text,
+    tag_name text
 )
