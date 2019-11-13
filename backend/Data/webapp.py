75cfbd4384b9035f301447be47e9b4662e7c503a
yime2 <yime2@cmc307-09.mathcs.carleton.edu>
2019-11-12 19:35:09 -0600

    taking out the hardcode part

--- backend/Data/webapp.py
+++ backend/Data/webapp.py
@@ -27,9 +27,9 @@ def fruit():
                            fruits=myFruit)
 
 
-@app.route('/midresults.html?firstbook=Lost&secondbook=The+Host', methods=['POST','GET']) #added the methods=... part with Andy
+@app.route('/midresults', methods=['POST','GET']) #added the methods=... part with Andy
 def midresultsBooks():
-    #if request.method == 'POST': #added the 'POST' and if statement. This code is from his flask app that I have in email. We can look at this for help.
+    if request.method == 'POST': #added the 'POST' and if statement. This code is from his flask app that I have in email. We can look at this for help.
     
 #        result = request.form
 #        firstbook = result['firstbook']
